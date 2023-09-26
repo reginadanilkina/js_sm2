267d38c9598ef7e5762f96c5f0940010be8b6364
reginadanilkina <146034775+reginadanilkina@users.noreply.github.com>
2023-09-26 16:20:59 +0300

    Add files via upload

---
+++ sm2/js/main.js
@@ -0,0 +1,5 @@
+let nameCity = prompt('Название Города');
+let createCity = prompt('Год образования');
+let peopleInCity = prompt('Население');
+
+alert('Городу ' + nameCity + ' исполнилось '+ `${2023 - createCity}` +' лет с момента его образования. Население - '+ peopleInCity +' человек')
